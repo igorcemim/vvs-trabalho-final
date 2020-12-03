aa256a5622846ae87c5b0bb44fd8ff1b6f0fdd6c
Igor Cemim <igor@cemim.com.br>
2020-12-03 04:44:11 -0300

    Add pipeline

--- src/test/java/br/com/gerenciadorproposta/GerenciadorpropostaApplicationTests.java
+++ src/test/java/br/com/gerenciadorproposta/GerenciadorpropostaApplicationTests.java
@@ -1,11 +1,16 @@
 package br.com.gerenciadorproposta;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class GerenciadorpropostaApplicationTests {
 
-
+    @Test
+    public void failed() {
+        Assertions.assertEquals(true, true);
+    }
 
 }
