e4f297dfc60364fef6c689f033f9ac1b14de0bda
Igor Cemim <igor@cemim.com.br>
2020-12-05 03:20:31 -0300

    Simplificada a lógica da abstração para testes funcionais e criadas variaveis de configuracao

--- src/test/java/br/com/gerenciadorproposta/functional/BaseFunctionalTest.java
+++ src/test/java/br/com/gerenciadorproposta/functional/BaseFunctionalTest.java
@@ -1,35 +1,40 @@
 package br.com.gerenciadorproposta.functional;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.time.Instant;
-
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.time.Instant;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseFunctionalTest {
-    protected static WebDriver driver;
+
+    protected WebDriver driver;
 
     @LocalServerPort
     protected int port;
 
+    @Value("${system.application.hostname}")
+    protected String applicationHostname;
+
+    @Value("${system.remote-web-driver.hostname}")
+    public String remoteWebDriverHostname;
+
     @BeforeAll
-    public static void setUp() throws Exception {
-        String hostname = getRemoteWebDriverHostname();
+    public void setUp() throws Exception {
         String port = "4444";
-        String url = String.format("http://%s:%s/wd/hub", hostname, port);
+        String url = String.format("http://%s:%s/wd/hub", remoteWebDriverHostname, port);
 
         ChromeOptions options = new ChromeOptions();
         driver = new RemoteWebDriver(new URL(url), options);
@@ -37,21 +42,7 @@ public abstract class BaseFunctionalTest {
     }
 
     protected String baseUrl() {
-        String hostname = getApplicationHostname();
-        return String.format("http://%s:%s", hostname, port);
-    }
-
-    private String getApplicationHostname() {
-        return isNetworkPerBuildEnabled() ? "build" : "localhost";
-    }
-
-    private static String getRemoteWebDriverHostname() {
-        return isNetworkPerBuildEnabled() ? "selenium-chrome-browser" : "localhost";
-    }
-
-    private static boolean isNetworkPerBuildEnabled() {
-        String networkPerBuild = System.getenv("FF_NETWORK_PER_BUILD");
-        return networkPerBuild != null && !networkPerBuild.equals("false") && !networkPerBuild.equals("0");
+        return String.format("http://%s:%s", applicationHostname, port);
     }
 
     @AfterEach
@@ -63,7 +54,7 @@ public abstract class BaseFunctionalTest {
     }
 
     @AfterAll
-    public static void tearDown() {
+    public void tearDown() {
         driver.quit();
     }
 
